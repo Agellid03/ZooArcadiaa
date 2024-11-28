f7f766a7bd067ab3a0b753a444e45a1d1330feba
Agellid03 <ouldbrahamsofiane@gmail.com>
2024-11-29 00:42:53 +0100

    Footer

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <h1>Bienvenue au Zoo d'Arcadia !</h1>
+            <p>Découvrez nos animaux, habitats, et services exceptionnels !</p>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ frontend/src/pages/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Services = () => {
+    return (
+        <div className="homepage">
+            <h1>Services</h1>
+          
+        </div>
+    );
+};
+
+export default Services;
