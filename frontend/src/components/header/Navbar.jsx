587a9e9bb672c0a9eef8b3750eb176f08355063a
Agellid03 <ouldbrahamsofiane@gmail.com>
2024-12-03 16:55:26 +0100

    Stylisation navbar et ajout d'element pour l'accessibilité

--- frontend/src/components/header/Navbar.jsx
+++ frontend/src/components/header/Navbar.jsx
@@ -8,19 +8,31 @@ import {
   NavbarToggler,
   NavItem,
 } from "reactstrap";
-// import "../header/clientNavbar.css";
+import "../header/navbar.css"
 
 const ClientNavbar = () => {
+  // État pour gérer l'ouverture/fermeture du menu burger
   const [isOpen, setIsOpen] = useState(false);
 
+  // Fonction pour basculer l'état
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <Navbar className="container_navbar" expand="md">
+      {/* Nom de la marque avec redirection vers la page d'accueil */}
       <NavbarBrand className="nav-brand" tag={NavLink} to="/">
         Zoo D'Arcadia
       </NavbarBrand>
-      <NavbarToggler aria-label="Toggle navigation" onClick={toggle} />
+      
+      {/* Bouton pour ouvrir/fermer le menu sur petits écrans */}
+      <NavbarToggler
+        className="navbar-toggler"
+        aria-label="Toggle navigation"
+        aria-expanded={isOpen}
+        onClick={toggle}
+      />
+      
+      {/* Menu de navigation */}
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ms-auto" navbar>
           <NavItem>
