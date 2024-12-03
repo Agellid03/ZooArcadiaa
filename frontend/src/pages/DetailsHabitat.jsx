cafbf27296c21be34ae466566e6ec24635cc8db0
Agellid03 <ouldbrahamsofiane@gmail.com>
2024-12-03 23:39:39 +0100

    Création page DetailsHabitat.jsx et stylisation

--- frontend/src/pages/DetailsHabitat.jsx
+++ frontend/src/pages/DetailsHabitat.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "../styles/detailsHabitat.css";
 
 const DetailHabitat = () => {
+  const { name } = useParams();
+  const [habitat, setHabitat] = useState(null);
+
+  useEffect(() => {
+    const fetchHabitat = () => {
+      // Données d'exemple statiques
+      const habitatData = [
+        {
+          name: "Savane",
+          imageUrl: "/images/Savane2.jpeg",  // Utilisation d'un chemin relatif vers 'public'
+          description: "La savane est un habitat vaste et ouvert.",
+        },
+        {
+          name: "Forêt",
+          imageUrl: "/images/Foret.jpeg",
+          description: "La forêt est un habitat riche en biodiversité.",
+        },
+        {
+          name: "Volière",
+          imageUrl: "/images/Voliere.jpeg",
+          description: "La volière abrite de nombreux oiseaux exotiques.",
+        },
+        {
+          name: "Aquarium",
+          imageUrl: "/images/Aquarium.jpeg",
+          description: "L'aquarium présente une grande variété d'espèces aquatiques.",
+        },
+      ];
+
+      // Trouver l'habitat correspondant au nom dans l'URL
+      const habitatFound = habitatData.find((habitat) => habitat.name.toLowerCase() === name.toLowerCase());
+
+      // Mise à jour de l'état avec les données de l'habitat trouvé
+      setHabitat(habitatFound);
+    };
+
+    fetchHabitat();
+  }, [name]);
+
+  // Vérification pour éviter d'accéder à des propriétés sur un objet null
+  if (!habitat) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Détails de l'Habitat</h1>
-      <p>Informations spécifiques sur un habitat donné.</p>
+    <div className="detail_habitat">
+      <h2>{habitat.name}</h2>
+      <img src={habitat.imageUrl} alt={habitat.name} className="habitat-image" />
+      <p>{habitat.description}</p>
     </div>
   );
 };
